177cd86b73c11a7ab5f4ebf91ce9225d4002ebc6
JesseLwz <jac31120@gmail.com>
2017-06-12 19:16:36 +0800

    PM2.5

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var linebot = require('linebot');
 var express = require('express');
+var getJSON = require('get-json');
 
 var bot = linebot({
     channelId: 1519666472,
@@ -7,23 +8,25 @@ var bot = linebot({
     channelAccessToken: 'SVXCMSo50NBIfXdzLpsVPDTNwJd9boEZSPM8bfRG/WPHZv9AEJE2W2mcx5OBOujFVv7gCLiLF0fkh2nKkmPKriRhYBZL7MJy4LYD3JNF6ZQarbTB7s9AM4i84Os7os9IeWupoFEA9a/YH6o0DSoZfgdB04t89/1O/w1cDnyilFU='
 });
 
-var jp;
-var timer2;
-_japan();//抓日幣匯率
+var timer;
+var pm = [];
+_getJSON();
 
-bot.on('message', function (event) {
-    if (event.message.type = 'text') {
-        var msg = '你剛說 : ' + event.message.text  + 'BTW 日匯率:'+jp;
-        event.reply(msg).then(function (data) {
-            // success 
-            console.log(msg);
-        }).catch(function (error) {
-            // error 
-            console.log('error');
-        });
-    }
-});
 
+// bot.on('message', function (event) {
+//     if (event.message.type = 'text') {
+//         var msg = '你剛說 : ' + event.message.text  + 'BTW 日匯率:'+jp;
+//         event.reply(msg).then(function (data) {
+//             // success 
+//             console.log(msg);
+//         }).catch(function (error) {
+//             // error 
+//             console.log('error');
+//         });
+//     }
+// });
+
+_bot();
 const app = express();
 const linebotParser = bot.parser();
 app.post('/', linebotParser);
@@ -34,23 +37,43 @@ var server = app.listen(process.env.PORT || 8080, function () {
     console.log("App now running on port", port);
 });
 
-function _japan() {
-  clearTimeout(timer2);
-  request({
-    url: "http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm",
-    method: "GET"
-  }, function(error, response, body) {
-    if (error || !body) {
-      return;
-    } else {
-      var $ = cheerio.load(body);
-      var target = $(".rate-content-sight.text-right.print_hide");
-      console.log(target[15].children[0].data);
-      jp = target[15].children[0].data;
-      if (jp < 0.28) {
-        bot.push('使用者 ID', '現在日幣 ' + jp + '，該買啦！');
+function _bot() {
+  bot.on('message', function(event) {
+    if (event.message.type == 'text') {
+      var msg = event.message.text;
+      var replyMsg = '';
+      if (msg.indexOf('PM2.5') != -1) {
+        pm.forEach(function(e, i) {
+          if (msg.indexOf(e[0]) != -1) {
+            replyMsg = e[0] + '的 PM2.5 數值為 ' + e[1];
+          }
+        });
+        if (replyMsg == '') {
+          replyMsg = '請輸入正確的地點';
+        }
       }
-      timer2 = setInterval(_japan, 120000);
+      if (replyMsg == '') {
+        replyMsg = '不知道「'+msg+'」是什麼意思 :p';
+      }
+
+      event.reply(replyMsg).then(function(data) {
+        console.log(replyMsg);
+      }).catch(function(error) {
+        console.log('error');
+      });
     }
   });
+}
+
+function _getJSON() {
+  clearTimeout(timer);
+  getJSON('http://opendata2.epa.gov.tw/AQX.json', function(error, response) {
+    response.forEach(function(e, i) {
+      pm[i] = [];
+      pm[i][0] = e.SiteName;
+      pm[i][1] = e['PM2.5'] * 1;
+      pm[i][2] = e.PM10 * 1;
+    });
+  });
+  timer = setInterval(_getJSON, 1800000); //每半小時抓取一次新資料
 }
