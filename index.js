70cc0e9c8ffdc74f63b0cda0035d411063533f66
JesseLwz <jac31120@gmail.com>
2017-09-13 10:52:58 +0800

    測試

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var pm = [];
 var jp;
 
 var rateArray = [];
-var titleArray =[];
+var titleArray = [];
 
 _getJSON();
 
@@ -46,14 +46,24 @@ function _bot() {
           replyMsg += e[0] + ',';
         });
       }
+      else if (msg.indexOf('匯率') != -1) {
+        rateArray.forEach(function (e, i) {
+          if (msg.indexOf(e[0]) != -1 || msg.indexOf(e[1]) != -1) {
+            replyMsg = e[0] + '的匯率為 ' + e[2];
+          }
+          if (replyMsg == '') {
+            replyMsg = '不懂';
+          }
+        });
+      }
       else if (msg.indexOf('日幣') != -1) {
         replyMsg = '目前日幣 ' + jp;
       }
       else if (msg.indexOf('測試') != -1) {
         if (titleArray.length < 1)
           replyMsg = '測試軌';
-        else 
-          replyMsg = titleArray[7];        
+        else
+          replyMsg = titleArray[7];
       }
       else if (msg.indexOf('屁孩') != -1) {
         var maxNum = 6;
@@ -131,15 +141,23 @@ function _japan() {
       //console.log(target[15].children[0].data);
       jp = target[15].children[0].data;
 
+      //TODO 幣別匯率做成 Array or JSON
+      var title = $(".currency.phone-small-font");
+      var rateName = $(".hidden-phone.print_show");
+      var decimal = $(".rate-content-sight.text-right.print_hide");
+      //titleArray.push(rateName[7].children[0].data);
+      for (var i = 0; i < title.length; i++) {
 
-       var title = $(".currency.phone-small-font");
-       var rateName = $(".hidden-phone.print_show");
-       var decimal = $(".rate-content-sight.text-right.print_hide");
-       titleArray.push(rateName[7].children[0].data);
-       for (var i = 0; i < title.length; i++) {
-         rateArray.push('{"' + rateName[i].children[0].data + '":[' + decimal[2 * i].children[0].data +']}');
-         titleArray.push(rateName[i].children[0].data);
-       }
+        var res = rateName[i].children[0].data.split("(")
+
+        rateArray[i] = [];
+        rateArray[i][0] = res[0].trimRight();  //中文
+        rateArray[i][1] = res[1].replace(")", ""); //簡英
+        rateArray[i][2] = decimal[2 * i].children[0].data;
+        
+        //rateArray.push('{"' + rateName[i].children[0].data + '":[' + decimal[2 * i].children[0].data + ']}');
+        //titleArray.push(rateName[i].children[0].data);
+      }
 
 
       if (jp < 0.275) {
