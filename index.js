b3b3b17c81505acc1e656f7d47148826ce3e65d1
JesseLwz <jac31120@gmail.com>
2018-02-27 14:31:06 +0800

    img api srtting

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function _bot() {
         exports.reply = function justReply(req, res) {
           var image_options = {
             method: "GET",
-            url: "https://api.imgur.com/3/album/jac21120/images",
+            url: "https://api.imgur.com/3/album/TeOvP/images",
             headers: {
               "Authorization": 'Client-ID 3c3846d8407e6a3'
             },
